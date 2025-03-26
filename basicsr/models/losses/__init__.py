3ace3eb6b492491d911a0d9e4f3487046ad3c2d6
dongw22 <dongw22@mcmaster.ca>
2025-03-26 00:01:40 -0400

    Delete basicsr directory

--- basicsr/models/losses/__init__.py
+++
@@ -1,5 +0,0 @@
-from .losses import (L1Loss, MSELoss, PSNRLoss, CharbonnierLoss, multi_VGGPerceptualLoss)
-
-__all__ = [
-    'L1Loss', 'MSELoss', 'PSNRLoss', 'CharbonnierLoss', 'multi_VGGPerceptualLoss'
-]
